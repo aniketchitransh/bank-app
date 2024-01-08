8ffa31260dceabb3a1f48ca5d9a8abdb5a93ac5a
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-08 15:57:39 +0530

    Jumped to button component within business component

--- bank_app/src/components/Business.jsx
+++ bank_app/src/components/Business.jsx
@@ -1,9 +1,22 @@
-import { features } from '../constants'
+import { features } from '../constants';
+import style, { layout } from '../style';
+import Button from './Button';
 
-const Business = () => {
-  return (
-    <div>Business</div>
-  )
-}
+const Business = () => (
+  <section id='features' className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={style.heading2}>
+        You do the business, <br className='sm:block hidden' /> we'll handle the money.
+      </h2>
+      <p className={`${style.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
+      <Button style='mt-10' />
+    </div>
+  </section>
+)
+
 
 export default Business
