ff987925c64e79ff9597cca53e5534bacb06cf2b
Aniket Chitransh <ani.chitransh@gmail.com>
2024-01-09 11:09:40 +0530

    Feedback and Testimonials completed, now moving to clients

--- bank_app/src/components/Testimonials.jsx
+++ bank_app/src/components/Testimonials.jsx
@@ -4,8 +4,7 @@ import FeedbackCard from './FeedbackCard'
 
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingX} ${styles.flexCenter} flex-col relative`}>
-    {/* TODO a gradient */}
-    <div />
+    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient' />
 
     <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
       <h1 className={styles.heading2}>
@@ -19,7 +18,7 @@ const Testimonials = () => (
       </div>
     </div>
 
-    <div className='felx flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+    <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
       {feedback.map((card) => (
         <FeedbackCard key={card.id} {...card} />
       ))}
